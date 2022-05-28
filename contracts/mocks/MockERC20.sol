4f962fb95185b7cfc0105cc97821fea6f10d7172
Alex Telegin <telegin.alexander@gmail.com>
2022-05-28 19:38:56 +0200

    Add decimals override for ERC-20 mocks

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -4,10 +4,20 @@ pragma solidity >=0.8.0;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
+
+    uint8 decimalsOverride;
+
     constructor(
-        string memory _name, 
-        string memory _symbol
-        ) ERC20(_name, _symbol) {}
+        string memory name_, 
+        string memory symbol_,
+        uint8 decimals_
+    ) ERC20(name_, symbol_) {
+        decimalsOverride = decimals_;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return decimalsOverride;
+    }
 
     function mint(address to, uint amount) public {
         _mint(to, amount);
--- test/test.ts
+++ test/test.ts
@@ -1,16 +1,14 @@
 const { ethers } = require("hardhat");
 const { waffle } = require("hardhat");
 import { expect, use } from "chai"
-import { solidity } from "ethereum-waffle"
+import Big from "big.js";
 
 import { 
   MirrahArt,
   MockERC20,
 } from "../types"
 
-use(solidity)
-
-describe("NFT", function() {
+describe("MirrahNFT", function() {
 
   // Factories
   let ERC20Factory: any
@@ -18,6 +16,10 @@ describe("NFT", function() {
 
   const artist = "0x709Ba910aF125285BC99d4D49e4381D49b3A4874"
   const developer = "0x3eb6Bf5B7AC2B683c787f7aac59683A8d05d885d"
+
+  const initialTokens = Big(10_000)
+  const usdcDecimals = 6
+  const daiDecimals = 18
 
   var mirrah: MirrahArt;
   var usdc: MockERC20;
@@ -35,23 +37,34 @@ describe("NFT", function() {
   });
 
   async function deployTokens() {
-    usdc = await ERC20Factory.deploy("USDC", "USDC")
-    await usdc.deployed()
-    await usdc.mint(wallet.address, "1000000000000000000000000")
-    dai = await ERC20Factory.deploy("DAI", "DAI")
-    await dai.deployed()
-    await dai.mint(wallet.address, "1000000000000000000000000")
+    usdc = await ERC20Factory.deploy("USDC", "USDC", usdcDecimals)
+    await usdc.mint(wallet.address, initialTokens.mul(Math.pow(10, usdcDecimals)).toFixed())
+    dai = await ERC20Factory.deploy("DAI", "DAI", daiDecimals)
+    await dai.mint(wallet.address, initialTokens.mul(Math.pow(10, daiDecimals)).toFixed())
     mirrah = await MirrahArtFactory.deploy(
       artist,
       developer,
       usdc.address,
       dai.address
     )
-    await mirrah.deployed()
+    console.log("Mirrah address: " + mirrah.address)
   }
 
-  it("Fetch number of minted tokens", async function() {
+  it("Correct number of initial tokens is minted", async function() {
     const count = await mirrah.balanceOf(mirrah.address)
-    console.log("Token count: " + count)
+    expect(count).to.eq(30)
+  })
+
+  async function checkBalances() {
+    const daiBalance = await dai.balanceOf(wallet.address)
+    console.log("Dai balance: " + daiBalance)
+    const usdcBalance = await usdc.balanceOf(wallet.address)
+    console.log("USDC balance: " + usdcBalance)
+  }
+
+  it("Correct number of initial tokens is minted", async function() {
+    await mirrah.attach(wallet.address).buyFromContract(0, 0)
+    await checkBalances()
+    // expect(await mirrah.ownerOf(0)).to.eq(wallet.address)
   })
 })
